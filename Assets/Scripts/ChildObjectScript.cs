9508482f64e1a9a7a9f7f43ea45c6bbfc9955192
MNKRCT <100119014+Karacete@users.noreply.github.com>
2024-08-20 00:48:23 +0300

    Yapılış-10
    Puan sistemi üzerine çalışıldı.

--- Assets/Scripts/ChildObjectScript.cs
+++ Assets/Scripts/ChildObjectScript.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ChildObjectScript : MonoBehaviour
 {
     [SerializeField]
     private GameObject losePanel;
-        private void OnTriggerEnter(Collider other)
+    private int pointCount;
+    [SerializeField]
+    private TextMeshProUGUI pointText; 
+    private void Start()
     {
-        if (other.gameObject.CompareTag("Barrier"))
+        pointCount = 0;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Barrier") || other.gameObject.CompareTag("Vehicle"))
         {
             Time.timeScale = 0;
             losePanel.SetActive(true);
+        }
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            pointCount += 4;
+            pointText.text = pointCount.ToString();
         }
     }
 }
