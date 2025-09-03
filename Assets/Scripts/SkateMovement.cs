07c9949959b8f7ec53f47c4063c8ac21b2712ee2
MNKRCT <muhammedkrct@gmail.com>
2025-09-03 13:34:46 +0300

    Mobil için dokunmatik kontroller eklenmiştir.

--- Assets/Scripts/SkateMovement.cs
+++ Assets/Scripts/SkateMovement.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class SkateMovement : MonoBehaviour
@@ -14,31 +15,39 @@ public class SkateMovement : MonoBehaviour
     void Start()
     {
         speed = 20;
-        horizontalSpeed = 5;
+        horizontalSpeed = 10;
         skateboardAnim = GetComponent<Animator>();
         uiManager = GameObject.FindWithTag("UI").GetComponent<UIManager>();
     }
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.A))
+        if (Input.touchCount > 0)
         {
-            skateboardAnim.SetBool("isLeft", true);
-            transform.Translate(Vector3.left * horizontalSpeed * Time.deltaTime);
-        }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            skateboardAnim.SetBool("isLeft", false);
-        }
+            Touch touch = Input.GetTouch(0);
+            float halfScreenWidth = Screen.width / 2;
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (touch.position.x > halfScreenWidth)
+                    {
+                        Right();
+                    }
+                    else if (touch.position.x < halfScreenWidth)
+                    {
+                        Left();
+                    }
+                    break;
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            skateboardAnim.SetBool("isRight", true);
-            transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            skateboardAnim.SetBool("isRight", false);
+                case TouchPhase.Ended:
+
+                case TouchPhase.Canceled:
+                    if (touch.position.x > halfScreenWidth)
+                        StopRight();
+                    else if (touch.position.x < halfScreenWidth)
+                        StopLeft();
+                    break;
+            }
         }
     }
     private void FixedUpdate()
@@ -48,15 +57,33 @@ public class SkateMovement : MonoBehaviour
         transform.position = pos;
         SpeedUp();
     }
+    private void Left()
+    {
+        skateboardAnim.SetBool("isLeft", true);
+        transform.Translate(Vector3.left * horizontalSpeed * Time.deltaTime);
+    }
+    private void StopLeft()
+    {
+        skateboardAnim.SetBool("isLeft", false);
+    }
+    private void Right()
+    {
+        skateboardAnim.SetBool("isRight", true);
+        transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
+    }
+    private void StopRight()
+    {
+        skateboardAnim.SetBool("isRight", false);
+    }
     private void SpeedUp()
     {
         if (uiManager.IsSkateboardSpeedup)
         {
             speed += 5;
-            horizontalSpeed += .8f;
+            horizontalSpeed += 1;
         }
     }
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Vehicle"))
         {
